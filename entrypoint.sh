bfd12dbc11501157e965930076f0e4b91fbcf839
Catalin Gheonea <catalin.gheonea@gmail.com>
2021-09-13 10:15:44 +0100

    fixed static aws region

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,4 +21,4 @@ echo "/usr/local/bin/kubectl" >> $GITHUB_PATH
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
-skaffold run --default-repo ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-1.amazonaws.com
+skaffold run --default-repo ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
